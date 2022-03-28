c1fd44702b8d539676efb15d0209e209ad8c0782
Gambit142 <nzefrancismaryeugorji@gmail.com>
2022-03-28 23:52:22 +0100

    Install react-router-dom for project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Headline from './components/Headline';
+import Navbar from './components/Navbar';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Headline />} />
+      </Routes>
+    </Router>
   );
 }
 
